632b0ffaf41fb5701c1acdb769027d0f83791bd6
Saharsha Bajracharya <bajracharyas442@gmail.com>
2025-02-12 09:43:40 +0545

    Added playfair cipher

--- one_time_pad.py
+++ one_time_pad.py
@@ -10,5 +10,23 @@ def encrypt(text  ,key):
         else:
             char = chr(((ord(p) - 97) + (ord(k)-97)) % 26 + 97)
             result += char
-    return result     
-print(encrypt("hello","write"))
+    return result 
+
+def decrupt(text  ,key):
+    result = ""
+    for i in range(len(text)):
+        p = text[i]
+        k = key[i]
+        if p.isupper() and k.isupper():
+            char = chr((ord(p) - ord(k)) % 26 + 65)
+            result += char
+        else:
+            char = chr(((ord(p) - 97) - (ord(k)-97)) % 26 + 97)
+            result += char
+    return result 
+
+cipher = encrypt("hello","write")
+
+print("Cipher text = ",cipher)
+
+print("decryption = ",decrupt(cipher,"write"))
